4ac445682f4c6c1ab24a3313595dc8f9c1672ded
Roman Baranov <baranov.r.p@gmail.com>
2017-01-21 18:07:08 +0100

    Implement search results
    Add methods to work with component
    Add steps to test
    Add assertion for test

--- src/test/java/com/akamai/JobSearchTest.java
+++ src/test/java/com/akamai/JobSearchTest.java
@@ -1,5 +1,7 @@
 package com.akamai;
 
+import java.util.List;
+
 import org.openqa.selenium.support.PageFactory;
 
 import org.testng.annotations.BeforeMethod;
@@ -7,6 +9,9 @@ import org.testng.annotations.Test;
 
 import com.akamai.components.SearchResults;
 import com.akamai.pages.JobSearch;
+
+import static java.lang.Integer.parseInt;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class JobSearchTest extends TestNgTestBase {
 
@@ -23,5 +28,11 @@ public class JobSearchTest extends TestNgTestBase {
 
     SearchResults searchResults =
       homePage.searchPanel().searchFor("test", "Krakow");
+    int numberOfResults = parseInt(searchResults.getNumberOfResults().trim());
+    assertThat(numberOfResults).isEqualTo(11);
+
+    List<String> roles =
+      searchResults.getRoleTitlesContains("Software Development Engineer in Test");
+    assertThat(roles).hasSize(4);
   }
 }
--- src/test/java/com/akamai/components/SearchResults.java
+++ src/test/java/com/akamai/components/SearchResults.java
@@ -1,16 +1,44 @@
 package com.akamai.components;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.akamai.pages.Page;
+
+import static org.openqa.selenium.By.cssSelector;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfAllElementsLocatedBy;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 /**
  * @author baranov.r.p
  */
 public class SearchResults extends Page {
 
+  private static final String CONTENT = "#portal_content";
+  private static final String NUMBER_OF_RESULTS = CONTENT + " .total_results";
+  private static final String ALL_ELS = CONTENT + " .full_content .job_list_row";
+
+
   public SearchResults(WebDriver webDriver) {
     super(webDriver);
+  }
+
+  public String getNumberOfResults() {
+    return wait.until(visibilityOfElementLocated(cssSelector(NUMBER_OF_RESULTS))).getText();
+  }
+
+  public List<WebElement> getAllResultsOnPage() {
+    return wait.until(visibilityOfAllElementsLocatedBy(cssSelector(ALL_ELS)));
+  }
+
+  public List<String> getRoleTitlesContains(String text) {
+    return getAllResultsOnPage().stream()
+      .map(el -> el.findElement(cssSelector("a")).getText().trim())
+      .filter(el -> el.toLowerCase().contains(text.toLowerCase()))
+      .collect(Collectors.toList());
   }
 
 }
