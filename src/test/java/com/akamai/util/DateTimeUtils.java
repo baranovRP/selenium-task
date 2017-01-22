71fbea2a680f102df208468cee2210bbda42347c
Roman Baranov <baranov.r.p@gmail.com>
2017-01-22 18:11:34 +0100

    Add locale to date formatter

--- src/test/java/com/akamai/util/DateTimeUtils.java
+++ src/test/java/com/akamai/util/DateTimeUtils.java
@@ -2,6 +2,7 @@ package com.akamai.util;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 /**
  * @author baranov.r.p
@@ -9,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 public final class DateTimeUtils {
 
   public static LocalDate parseToDate(String text) {
-    return LocalDate.parse(text, DateTimeFormatter.ofPattern("MMM dd, uuuu"));
+    return LocalDate.parse(text,
+      DateTimeFormatter.ofPattern("MMM dd, yyyy").withLocale(Locale.UK));
   }
 }
